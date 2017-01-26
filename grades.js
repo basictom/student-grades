e682f71ad7c5c2c59e9d0e7fbadaeedaa5666010
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-01-25 20:04:08 -0600

    adding html/js

--- grades.js
+++ grades.js
@@ -11,11 +11,26 @@
 // Use console.log to output the following criteria:
 //
 // How many of each grade?
-// What is the lowest grade?
-// What is the highest grade?
-
-
-
 
 var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+for(var i=0; i< scores.length; i++) {
+  if ((scores[i] >= 50) && (scores[i] <= 60)){
+    gradeF += 1;
+  }else if((scores[i] >= 61) && (scores[i] <= 70)){
+    gradeD += 1;
+  }else if((scores[i] >= 71) && (scores[i] <= 80)){
+    gradeC += 1;
+  }else if((scores[i] >= 81) && (scores[i] <= 90)){
+    gradeB += 1;
+  }else if((scores[i] >= 91) && (scores[i] <= 100)){
+    gradeA += 1;
+  }else{
+    alert("No grades here.");
+  }
+};
 console.log(scores);
+
+
+// What is the lowest grade?
+// What is the highest grade?
